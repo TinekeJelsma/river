eecdaf176b5817d85b0d1ba733ea33408dc36588
Tineke <tinekejelsma@hotmail.com>
2021-02-04 11:31:09 +0100

    run base method with newer evaluation file

--- river/test_temporarily.py
+++ river/test_temporarily.py
@@ -8,38 +8,53 @@ from river import compose
 from river import optim
 from river import datasets
 from river import linear_model
+from river import compat
 import itertools
+from sklearn import linear_model
+
+max_samples = 1500
+n_features = 5
+n_centroids = 5
 streams = []
 
 for x in range(1):
     # base negative
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[1,0]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[1, 0]))
     # base positive
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[0,1]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[0, 1]))
     # drift negative
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0.5, n_drift_centroids=1, n_centroids=1, class_weights=[1,0]))
+    # streams.append(synth.RandomRBFDrift(seed_model=30, seed_sample=12, n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[1,0]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[1, 0]))
     # drift positive
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[0,1]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[0, 1]))
     # drift negative
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0.5, n_drift_centroids=1, n_centroids=1, class_weights=[1,0]))
+    # streams.append(synth.RandomRBFDrift(seed_model=30, seed_sample=12, n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[1,0]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[1, 0]))
     # drift positive
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=1, change_speed=0, n_drift_centroids=1, n_centroids=1, class_weights=[0,1]))
+    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
+                                        class_weights=[0, 1]))
 
-# self defeating influence
-X_y = synth.PredictionInfluenceStream(stream= streams, weight_incorrect=1.02, weight_correct=0.98, weight_update = 1, weight = [1,1,0,0,0,0])
-
-# self fulfilling influence
-# X_y = synth.PredictionInfluenceStream(stream= streams, weight_incorrect=0.99, weight_correct=1.01, weight_update = 1)
-# X_y = synth.RandomRBFDrift(seed_model=30, seed_sample=30, n_classes=2, n_features=4, change_speed=0, n_drift_centroids=10, n_centroids=20, class_weights=[0.5,0,5])
+X_y = synth.PredictionInfluenceStream(stream=streams, weight_incorrect=1.02, weight_correct=0.98, weight_update=1,
+                                      weight=[1, 1,0,0,0,0])
 # X_y = datasets.LendingClub()
 # max_samples=15384462
 # for x, y in X_y.take(5):
 #     print(x, y)
-# model = preprocessing.StandardScaler()
-# model = tree.HoeffdingAdaptiveTreeClassifier(grace_period=100, split_confidence=1e-5, leaf_prediction='nb', nb_threshold=10, seed=0)
 
-model = preprocessing.StandardScaler() 
-model |= linear_model.ALMAClassifier()
+
+model = preprocessing.StandardScaler()
+model |= compat.convert_sklearn_to_river(
+    estimator=linear_model.SGDClassifier(loss='log', eta0=0.01, learning_rate='optimal'), classes=[0, 1])
+hidden_model = preprocessing.StandardScaler()
+hidden_model |= compat.convert_sklearn_to_river(
+    estimator=linear_model.SGDClassifier(loss='log', eta0=0.01, learning_rate='optimal'), classes=[0, 1])
+
 metric = metrics.Accuracy()
+hidden_metric = metrics.Accuracy()
 
-evaluate.evaluate_influential(X_y, model, metric, print_every=10000, comparison_block = 1000, intervals = 8, max_samples=5000, prior = 1)
+evaluate.evaluate_influential(X_y, model= model, hidden_model = hidden_model, metric = metric, hidden_metric = hidden_metric, print_every=100, comparison_block = 1000, intervals = 8, max_samples=5000, prior = 1)
