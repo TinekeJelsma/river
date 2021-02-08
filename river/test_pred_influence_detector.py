ee6433d34471b4e9b1020d490a7d308f1bbf2ec8
Tineke <tinekejelsma@hotmail.com>
2021-02-08 17:04:16 +0100

    changed some settings =

--- river/test_pred_influence_detector.py
+++ river/test_pred_influence_detector.py
@@ -13,23 +13,23 @@ from river import compat
 import itertools
 
 streams = []
-max_samples = 200
+max_samples = 10000
 n_features = 1
 n_centroids = 1
 
 for x in range(5):
     # negative
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=1, n_drift_centroids=1, n_centroids=n_centroids,
+    streams.append(synth.RandomRBFDrift(seed_model = 123 + x, n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
                                         class_weights=[1, 0]))
     # positive
-    streams.append(synth.RandomRBFDrift(n_classes=2, n_features=n_features, change_speed=1, n_drift_centroids=1, n_centroids=n_centroids,
+    streams.append(synth.RandomRBFDrift(seed_model = 225 + x, n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
                                         class_weights=[0, 1]))
 
-X_y = synth.PredictionInfluenceStream(stream=streams, weight_incorrect=1.01, weight_correct=0.99, weight_update=1, weight_update_delay=100)
+X_y = synth.PredictionInfluenceStream(stream=streams, weight_incorrect=1.01, weight_correct=0.99, weight_update=1, weight_update_delay=100, weight=[1,1,0,0,0,0,0,0,0,0])
 model = preprocessing.StandardScaler()
 model |= compat.convert_sklearn_to_river(
     estimator=linear_model.SGDClassifier(loss='log', eta0=0.01, learning_rate='optimal'), classes=[0, 1])
 
 metric = metrics.Accuracy()
 
-evaluate.evaluate_influential(X_y, model= model, metric = metric, print_every=100, comparison_block = 1000, intervals = 8, max_samples=max_samples, prior = 1, batch_size=1)
+evaluate.evaluate_influential(X_y, model= model, metric = metric, print_every=100, comparison_block = 1000, max_samples=max_samples, prior = 1, batch_size=1)
