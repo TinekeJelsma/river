4bfc3d55d4618f6d27a224bb2f7711ddcf77ac86
Tineke <tinekejelsma@hotmail.com>
2021-02-10 16:53:13 +0100

    created trainingset for kde

--- river/evaluate/prediction_influence.py
+++ river/evaluate/prediction_influence.py
@@ -36,21 +36,17 @@ def evaluate_influential(dataset: base.typing.Stream, model, metric: metrics.Met
 
     # Determine if predict_one or predict_proba_one should be used in case of a classifier
     pred_func = model.predict_one
-    if utils.inspect.isclassifier(model) and not metric.requires_labels:
-        pred_func = model.predict_proba_one
+    # if utils.inspect.isclassifier(model) and not metric.requires_labels:
+    #     print('predict proba')
+    #     pred_func = model.predict_proba_one
 
     preds = {}
     chunk_tracker = 0
     cm = metrics.ConfusionMatrix()
+    cache = pd.DataFrame(columns=['timestamp', 'x', 'ypred', 'ytrue'])
     TP, FP, FN, TN = [], [], [], []
-    cm_values = [TP, FP, FN, TN]
-    cm_names = ['TP', 'FP', 'FN', 'TN']
-    hist_info = {}
-
     pos_yvalues =  [[] for _ in range(5)]  
     neg_yvalues =  [[] for _ in range(5)]  
-    pvaluePos =  [[] for _ in range(5)]  
-    pvalueNeg =  [[] for _ in range(5)]
     pos_xvalues, neg_xvalues = [], []
 
     drift_detector_positive = drift.ADWIN()
@@ -82,19 +78,6 @@ def evaluate_influential(dataset: base.typing.Stream, model, metric: metrics.Met
             if isinstance(dataset, PredictionInfluenceStream):
                 # update weights of stream based on classification
                 dataset.receive_feedback(y_true=y, y_pred=y_pred, x_features=x)
-            # put feature values in 4 bins (cm bins)
-            if y_pred == 1 and y == 1:
-                # true positive
-                TP.append(x)
-            if y_pred == 1 and y == 0:
-                # false positive
-                FP.append(x)
-            if y_pred == 0 and y == 1:
-                # false negative
-                FN.append(x)
-            if y_pred == 0 and y == 0:
-                # true negative
-                TN.append(x)
 
             if y == 1:
                 key_number = 0
@@ -123,6 +106,19 @@ def evaluate_influential(dataset: base.typing.Stream, model, metric: metrics.Met
                         drift_detector_negative.reset()
                     key_number += 1
 
+        # add instance to cache and remove too old instances
+        if len(cache.index) < comparison_block:
+            cache.loc[i] = pd.Series({'timestamp': i, 'x': x, 'ypred': y_pred, 'ytrue': y})
+        else:
+            index = i - comparison_block
+            cache = cache.drop(index = index)
+            cache.loc[i] = pd.Series({'timestamp': i, 'x': x, 'ypred': y_pred, 'ytrue': y})
+
+        # if y is not None and y_pred is not None:
+        subset = cache.loc[(cache['ytrue'] == y) & (cache['ypred'] == y_pred)]
+        print(f'subset = {subset}, and ypred = {y_pred} y = {y}')
+
+
         if n_total_answers < batch_size or batch_size == 1:
             # if the model learns online, learn one, or when the index is below the first batch size, learn one
             model.learn_one(x=x, y=y)
@@ -145,85 +141,6 @@ def evaluate_influential(dataset: base.typing.Stream, model, metric: metrics.Met
                 msg += f' – {model._memory_usage}'
             print(msg, **print_kwargs)
 
-        if n_total_answers % comparison_block == 0:
-            # fill all bins by looping over keys in feature dict
-            feature_names = list(x.keys())
-            feature_number = 0
-            for feature in feature_names:
-                index = 0
-                for cm_value in cm_values:
-                    feature_values = [value.get(feature) for value in cm_value]
-                    if all(isinstance(feature_value, str) for feature_value in feature_values):
-                        # this is a categorical feature
-                        dict_name = cm_names[index] + '-' + str(chunk_tracker) + '-' + str(feature_number)
-                        hist_info[dict_name] = {}
-                        hist_info[dict_name]['classification'] = cm_names[index]
-                        hist_info[dict_name]['chunk'] = chunk_tracker
-                        hist_info[dict_name]['feature'] = feature_number
-                        hist_info[dict_name]['values'] = feature_values
-                    else:
-                        # this is a numerical feature
-                        dict_name = cm_names[index] + '-' + str(chunk_tracker) + '-' + str(feature_number)
-                        hist_info[dict_name] = {}
-                        hist_info[dict_name]['classification'] = cm_names[index]
-                        hist_info[dict_name]['chunk'] = chunk_tracker
-                        hist_info[dict_name]['feature'] = feature_number
-                        hist_info[dict_name]['values'] = feature_values                   
-                    index += 1
-                feature_number += 1
-
-            chunk_tracker += 1
-            # empty bins
-            TP, FP, FN, TN = [], [], [], []
-            cm_values = [TP, FP, FN, TN]
-
-        if chunk_tracker >= 2 and n_total_answers % comparison_block == 0 and drift_detection is None:
-            # this means we can start comparing the densities
-            first_chunk = chunk_tracker - 2
-            second_chunk = chunk_tracker - 1
-            density_comparison = {}
-            for classification in cm_names:
-                for feature in range(len(x)):
-                    name_first = classification + '-' + str(first_chunk) + '-' + str(feature)
-                    name_second = classification + '-' + str(second_chunk) + '-' + str(feature)
-                    first_dist = np.array(hist_info[name_first].get('values')).reshape(-1,1)
-                    second_dist = np.array(hist_info[name_second].get('values')).reshape(-1,1)
-                    if len(first_dist) > 10 and len(second_dist) > 1:
-                        params = {'bandwidth': np.logspace(-1, 1, 10)}
-                        grid = GridSearchCV(KernelDensity(), params)
-                        grid.fit(first_dist)
-                        kde = grid.best_estimator_
-                        # kde1 = KernelDensity(kernel='gaussian', bandwidth=0.75).fit(first_dist)
-                        log_dens = kde.score_samples(second_dist)
-                        band_width = grid.best_estimator_.bandwidth
-                    elif len(first_dist) > 1 and len(second_dist) > 1:
-                        kde1 = KernelDensity(kernel='gaussian', bandwidth=0.75).fit(first_dist)
-                        log_dens = kde1.score_samples(second_dist)
-                        band_width = 0.75
-                    else: 
-                        log_dens = None
-                        band_width = None
-                    density_comparison[name_first] = {}
-                    density_comparison[name_first] = {'score samples': log_dens, 'bandwidth': band_width}
-            
-            # compare TP and FN, and TN and FP
-            for feature in range(len(x)):
-                TP_scores = 'TP-' + str(first_chunk) + '-' + str(feature)
-                FN_scores = 'FN-' + str(first_chunk) + '-' + str(feature)
-                if density_comparison[TP_scores].get('score samples') is not None and density_comparison[FN_scores].get('score samples') is not None:
-                    result = kstest(density_comparison[TP_scores].get('score samples'), density_comparison[FN_scores].get('score samples'))
-                    print(f'difference between TP and FN in feature {feature} is {result}')
-                else:
-                    print('something was non existent')
-
-                TN_scores = 'TN-' + str(first_chunk) + '-' + str(feature)
-                FP_scores = 'FP-' + str(first_chunk) + '-' + str(feature)
-                if density_comparison[TN_scores].get('score samples') is not None and density_comparison[FP_scores].get('score samples') is not None:
-                    result = kstest(density_comparison[TN_scores].get('score samples'), density_comparison[FP_scores].get('score samples'))
-                    print(f'difference between TN and FP in feature {feature} is {result}')
-                else:
-                    print('something was NOne')
-
         if n_total_answers == max_samples:
             # visualize the feature space in plots
             for i in range(len(x)):
@@ -244,6 +161,7 @@ def evaluate_influential(dataset: base.typing.Stream, model, metric: metrics.Met
 
         if n_total_answers >= max_samples:
             print(cm)
+            print(cache)
             if isinstance(dataset, PredictionInfluenceStream):
                 print(dataset.stream_weight)
             return metric
--- river/test_pred_influence_detector.py
+++ river/test_pred_influence_detector.py
@@ -13,11 +13,11 @@ from river import compat
 import itertools
 
 streams = []
-max_samples = 10000
+max_samples = 10
 n_features = 1
 n_centroids = 1
 
-for x in range(5):
+for x in range(2):
     # negative
     streams.append(synth.RandomRBFDrift(seed_model = 123 + x, n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
                                         class_weights=[1, 0]))
@@ -25,11 +25,11 @@ for x in range(5):
     streams.append(synth.RandomRBFDrift(seed_model = 225 + x, n_classes=2, n_features=n_features, change_speed=0, n_drift_centroids=1, n_centroids=n_centroids,
                                         class_weights=[0, 1]))
 
-X_y = synth.PredictionInfluenceStream(stream=streams, weight_incorrect=1.01, weight_correct=0.99, weight_update=1, weight_update_delay=100, weight=[1,1,0,0,0,0,0,0,0,0])
+X_y = synth.PredictionInfluenceStream(stream=streams, weight_incorrect=1.01, weight_correct=0.99, weight_update=1, weight_update_delay=100, weight=[1,1,0,0])
 model = preprocessing.StandardScaler()
 model |= compat.convert_sklearn_to_river(
     estimator=linear_model.SGDClassifier(loss='log', eta0=0.01, learning_rate='optimal'), classes=[0, 1])
 
-metric = metrics.Accuracy()
+metric = metrics.ROCAUC()
 
-evaluate.evaluate_influential(X_y, model= model, metric = metric, print_every=100, comparison_block = 1000, max_samples=max_samples, prior = 1, batch_size=1)
+evaluate.evaluate_influential(X_y, model= model, metric = metric, print_every=100, comparison_block = 5, max_samples=max_samples, prior = 1, batch_size=1)
